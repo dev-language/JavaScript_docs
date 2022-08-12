7310196acb2a12251806bef27dcfc002420b5b0b
SEOGUN <ksj083895@gmail.com>
2022-08-12 17:43:49 +0500

    할당 연산자

---
+++ 4.assignment.js
@@ -0,0 +1,31 @@
+// 할당연산자 (Assignment operator)
+// 하나의 equal sign(=) 만 사용하면 할당연산자를 사용할 수 있다.
+
+let a = 1;
+a = a + 2;
+console.log(a); //3
+
+// 산술연산자 축약 버전
+
+// +=
+a += 3;
+console.log(a); // 3+3 = 6
+
+// -=
+a -= 3;
+console.log(a); // 6-3 = 3
+
+// *=
+a *= 3;
+console.log(a); // 3*3 = 9
+
+// /=
+a /= 2;
+console.log(a); // 9/2 = 4.5
+
+a **= 2;
+console.log(a); // 4.5 * 4.5 = 20.25
+
+// *=
+a %= 2;
+console.log(a); // 20.25 % 2 = 0.25
