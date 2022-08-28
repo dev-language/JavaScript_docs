fcfbb0ff2c2617d94a1390945241bd57d781cd44
SEOGUN <ksj083895@gmail.com>
2022-08-28 10:27:42 +0500

    객체 축약

---
+++ 5. object/3.shorts.js
@@ -0,0 +1,13 @@
+const x = 0;
+const y = 0;
+const coordinate = { x: x, y: x };
+// x라는 키 이름과 참조하고 있는 변수 이름이 동일하다면 x:x, y:y 대신 값을 생량할 수 있다.
+//contst coordinate = { x, y }
+
+function makeobj(name, age) {
+    return {
+        name,
+        age,
+    };
+}
+console.log(makeobj('서근', 20));
